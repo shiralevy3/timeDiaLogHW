8e7333d8ee13abcbac58e2e295496b736189e13f
shiralevy3 <shiralevy333@gmail.com>
2024-11-26 18:18:02 +0200

    הקוד עם הכפתור ועבוד הבחירת זמן

--- app/src/main/java/com/example/timedialoghw/MainActivity.java
+++ app/src/main/java/com/example/timedialoghw/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.timedialoghw;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonTimePicker;
+    private TextView textViewSelectedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setContentView(R.layout.activity_main);
+
+        buttonTimePicker = findViewById(R.id.btnTmPck);
+        textViewSelectedTime = findViewById(R.id.tvTime);
+
+        buttonTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get the current time
+                Calendar calendar = Calendar.getInstance();
+                int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                int currentMinute = calendar.get(Calendar.MINUTE);
+
+                //timePicker
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        MainActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                String formattedTime = String.format("%02d:%02d" , hourOfDay, minute);
+                                textViewSelectedTime.setText("The time that you selected is: " + formattedTime);
+
+
+                            }
+                        },currentHour,currentMinute , true
+
+                );
+                timePickerDialog.show();
+//showing:
+
+            }
+        });
+
+
+
     }
 }
